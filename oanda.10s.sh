7fad03af3094a7d1a37bb01cdbc455b832861ac7
Ryusuke Sekiguchi <tanukiti1987@gmail.com>
2016-04-10 02:14:37 +0900

    Fix fx status bitbar plugin

--- oanda.10s.sh
+++ oanda.10s.sh
@@ -1,16 +1,52 @@
 #!/usr/bin/env ruby
 require 'oanda_api'
 
-TARGET_PAIRS = %w(USD_JPY)
+class Integer
+  def jpy_comma
+    self.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
+  end
+end
+
+MAIN_PAIR = ["USD_JPY"]
+SUB_PAIRS = %w(EUR_USD)
 
 client = OandaAPI::Client::TokenClient.new(:practice, ENV["OANDA_DEMO_API_KEY"])
 
-prices = client.prices(instruments: TARGET_PAIRS).get
+price = client.prices(instruments: MAIN_PAIR).get.first
+puts "#{price.instrument.gsub(/_/, '')}: #{price.ask}"
+puts "---"
 
-prices.each do |price|
+prices = client.prices(instruments: SUB_PAIRS).get
+prices.each_with_index do |price, i|
   puts "#{price.instrument.gsub(/_/, '')}: #{price.ask}"
-  price.instrument       # => "EUR_USD"
-  price.ask              # => 1.13781
-  price.bid              # => 1.13759
-  price.time             # => 2015-01-27 21:01:13 UTC
+end
+
+accounts = client.accounts.get
+
+accounts.each do |a|
+  puts "#{a.account_name}"
+
+  account = client.account(a.account_id).get
+  puts "口座資産: ¥#{(account.balance).round.jpy_comma}"
+  puts "有効証拠金: ¥#{(account.balance + account.unrealized_pl).round.jpy_comma}"
+  puts "評価損益: ¥#{account.unrealized_pl.round.jpy_comma}"
+
+  begin
+    position = client.account(a.account_id).position(MAIN_PAIR).get
+    puts "#{price.instrument.gsub(/_/, '')}ポジション数: #{position.total_units.jpy_comma}"
+  rescue OandaAPI::RequestError
+    nil
+  end
+
+  unless SUB_PAIRS.empty?
+    prices = client.prices(instruments: SUB_PAIRS).get
+    prices.each_with_index do |price, i|
+      begin
+        position = client.account(a.account_id).position(price.instrument).get
+        puts "#{price.instrument.gsub(/_/, '')}ポジション数: #{position.total_units}"
+      rescue OandaAPI::RequestError
+        nil
+      end
+    end
+  end
 end
